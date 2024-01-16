9c5948269519bb4d7771360f4f0f1f596de74a08
joaopedromsantos <joaomartinscrf2019@gmail.com>
2024-01-15 23:43:46 -0300

    modification in navbar

--- front-end-MSS/src/components/Navbar.jsx
+++ front-end-MSS/src/components/Navbar.jsx
@@ -1,22 +1,70 @@
 import React from 'react'
 import './Navbar.css'
 
+import Foto from "../media/logo.png"
+import Foto_User from "../media/exemplo-foto.png"
+
+import { RxDashboard } from "react-icons/rx";
+import { BsBoxSeam } from "react-icons/bs";
+import { RiBrush2Line } from "react-icons/ri";
+import { TbCash } from "react-icons/tb";
+
+
+
 const navbar = () => {
   return (
     <div id='navbar'>
-        <div id='logo'></div>
+        <div id='logo'>
+          <img src={Foto} id='logo-imagem'></img>
+        </div>
 
         <div id='cont-header'>
-            <div id='box-icones'>
-                <div id='icon'></div>
-                <div id='word'></div>
-                <div id='hug'></div>
+            <div className='box-header'>
+                <div className='icon'>
+                  <RxDashboard />
+                </div>
+                <div className='word'>
+                  <p>Dashboard</p>
+                </div>
+                <div className='hub'>
+                </div>
+            </div>
+            <div className='box-header'>
+                <div className='icon'>
+                  <BsBoxSeam  />
+                </div>
+                <div className='word'>
+                  <p>Estoque</p>
+                </div>
+                <div className='hub'></div>
+            </div>
+            <div className='box-header'>
+                <div className='icon'>
+                  <RiBrush2Line    />
+                </div>
+                <div className='word'>
+                  <p>Marcações</p>
+                </div>
+                <div className='hub'></div>
+            </div>
+            <div className='box-header'>
+                <div className='icon'>
+                  <TbCash  />
+                </div>
+                <div className='word'>
+                  <p>Cobranças</p>
+                </div>
+                <div className='hub'></div>
             </div>
         </div>
 
         <div id='user-info'>
-            <div id='user-image'></div>
-            <div id='user-name'></div>
+            <div id='user-image'>
+              <img src={Foto_User} id='user-foto'></img>
+            </div>
+            <div id='user-name'>
+              <p>João Pedro</p>
+            </div>
         </div>
     </div>
   )
